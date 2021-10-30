924121d8d22fd0e426c65e4e68c70b45f93fb4ce
SumitK27 <sumitkajbaje@gmail.com>
2021-10-31 01:03:57 +0530

    Added SocketIO to the server

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
+const io = require("socket.io")(server);
 
 // Using Static contents from the public directory
 app.use(express.static("public"));
@@ -11,6 +12,12 @@ app.use(express.static("public"));
 // Home route
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
+});
+
+// Adding SocketIO connection to the server
+io.on("connection", (socket) => {
+    console.log("User connected to SocketIO server.");
+    console.log(socket.id);
 });
 
 // Server
